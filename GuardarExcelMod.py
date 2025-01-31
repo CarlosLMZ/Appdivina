47bb74b95d8d8612f1cac9fa1f06c92810d10602
CarlosLMZ <carloslm.ziegler@gmail.com>
2025-01-31 09:12:59 +0100

    Update GuardarExcelMod.py

--- GuardarExcelMod.py
+++ GuardarExcelMod.py
@@ -1,12 +1,12 @@
 import openpyxl as pxl
 
 def guardar_resultados(nombre, contador, victoria, derrota):
-    wb = pxl.load_workbook("C:\\Users\\Carlos\\Adivinanzas.xlsx")
+    wb = pxl.load_workbook("D:\TATE\A-Proyectos Data Analysis\A-Proyectos_Github\Appdivina\\Adivinanzas.xlsx")
     ws = wb["Juego_Stats"]
 
     if ws.max_row == 1:
         ws.append(["Nombre", "Intentos", "Victoria", "Derrota"])
-        wb.save("C:\\Users\\Carlos\\Adivinanzas.xlsx")
+        wb.save("D:\TATE\A-Proyectos Data Analysis\A-Proyectos_Github\Appdivina\\Adivinanzas.xlsx")
     
     ws.append([nombre, contador, victoria, derrota])
-    wb.save("C:\\Users\\Carlos\\Adivinanzas.xlsx")
+    wb.save("D:\TATE\A-Proyectos Data Analysis\A-Proyectos_Github\Appdivina\\Adivinanzas.xlsx")
